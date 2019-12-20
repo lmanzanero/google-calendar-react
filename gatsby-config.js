c2403764324ac2b7c92a814df02188d2b7ea5a14
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-12-19 21:18:33 -0600

    Changed title name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  pathPrefix: "/google-calendar-react",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Smart Smile Belize`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
